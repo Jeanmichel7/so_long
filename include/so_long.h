21e412c2ad9abb32a161c0235aa0b2eaad4439df
Jeanmichel7 <jeanmichel.rasser@laposte.net>
2022-04-10 02:27:52 +0200

    test

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: jrasser <jrasser@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 21:43:35 by jrasser           #+#    #+#             */
-/*   Updated: 2022/04/09 21:44:44 by jrasser          ###   ########.fr       */
+/*   Updated: 2022/04/10 02:03:21 by jrasser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,22 @@
 
 # include "mlx.h"
 # include <stdio.h>
+# include <stdlib.h>
+# include "../libft/libft.h"
+
+typedef struct s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*img_ptr;
+}	t_data;
+
+typedef struct s_img
+{
+	int		pixel_bits;
+	int		line_bytes;
+	int		endian;
+	char	*buffer;
+}	t_img;
 
 #endif
--- main.c
+++ main.c
@@ -6,34 +6,46 @@
 /*   By: jrasser <jrasser@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 18:15:20 by jrasser           #+#    #+#             */
-/*   Updated: 2022/04/09 21:54:45 by jrasser          ###   ########.fr       */
+/*   Updated: 2022/04/10 02:26:14 by jrasser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/so_long.h"
 
-/*
-void	*mlx_new_window (void *mlx_ptr, int size_x, int size_y, char *title);
-int		mlx_clear_window (void *mlx_ptr, void *win_ptr);
-int		mlx_destroy_window (void *mlx_ptr, void *win_ptr);
-
-*/
-
-int	deal_key(int key, void *param)
+int	deal_key(int key, t_data *data)
 {
-	printf("X\n");
+	ft_printf("%d\n", key);
+	if (key == 65307)
+	{
+		mlx_destroy_image(data->mlx_ptr, data->img_ptr);
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		mlx_destroy_display(data->mlx_ptr);
+		free(data->mlx_ptr);
+		exit(0);
+	}
+	return (0);
+}
+
+int	deal_mouse(int button, int x, int y, t_data *data)
+{
+	ft_printf("%d %d %d\n", button, x, y);
 	return (0);
 }
 
 int	main(void)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
+	t_data	data;
+	t_img	img;
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "title");
-	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xFFFFFF);
-	mlx_key_hook(win_ptr, deal_key, (void *)0);
-	mlx_loop(mlx_ptr);
+	data.mlx_ptr = mlx_init();
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 500, 500, "title");
+	mlx_pixel_put(data.mlx_ptr, data.win_ptr, 250, 250, 0xFFFFFF);
+	mlx_key_hook(data.win_ptr, deal_key, &data);
+	mlx_mouse_hook(data.win_ptr, deal_mouse, &data);
+
+	data.img_ptr = mlx_new_image(data.mlx_ptr, 50, 50);
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.img_ptr, 50, 50);
+
+	mlx_loop(data.mlx_ptr);
 	return (0);
 }
